c0de0f4466e9b972b7daff36ae30d7931c1e91eb
nivasan1 <nivasan@umich.edu>
2022-05-26 15:06:11 -0400

    WrappedEth

--- contracts/Comptroller.sol
+++ contracts/Comptroller.sol
@@ -1463,6 +1463,6 @@ contract Comptroller is ComptrollerV7Storage, ComptrollerInterface, ComptrollerE
      * @return The address of COMP
      */
     function getCompAddress() public view returns (address) {
-        return 0xc00e94Cb662C3520282E6f5717214004A7f26888;
+        return 0x14B3F74f86c4DE775112124c08CAf7a439f3083B;
     }
 }
